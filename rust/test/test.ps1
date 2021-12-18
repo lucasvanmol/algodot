cfd0abaff53d9c3908c85f84dbd781a7f0cb73e7
Lucas Van Mol <lucas.vanmol@gmail.com>
2021-12-17 23:51:34 -0800

    🧪Use custom config file for test network

--- rust/test/test.ps1
+++ rust/test/test.ps1
@@ -4,7 +4,7 @@ cargo build
 
 Copy-Item "../target/debug/algodot.dll" -Destination "./project/lib"
 
-# For now this is set manually with `./sandbox goal account export -a ...`
+# For now this is set manually with `./sandbox goal account export -a ...`, based on genesis.json
 $Env:ALGODOT_FUNDER_MNEMONIC = "version hand outdoor sting spawn warrior noise rose lift faint apart shaft clerk melt deal soft shop pool sister collect armed planet mixed absent unfair"
 
 & 'C:\Program Files (x86)\Steam\steamapps\common\Godot Engine\godot.windows.opt.tools.64.exe' --path ./project/
