75683bdee5cbbfe374a21ff629c368ca4be22603
Lucas Van Mol <lucas.vanmol@gmail.com>
2022-01-16 17:50:46 -0800

    Remove demo project

--- godot/Demo/SetUpShop.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_Setup_pressed():
-	get_node("/root/DemoGame").set_up_shop()
-
-	
-
-
--- godot/Demo/ShopItem.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends Node2D
-
-export(int, 283) var item setget set_item
-
-
-func set_item(to):
-	item = to
-	$AnimatedSprite.frame = to
