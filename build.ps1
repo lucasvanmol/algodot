494756d24d51b8588f072404250e39acca3b510e
Lucas Van Mol <lucas.vanmol@gmail.com>
2022-02-02 22:10:26 -0800

    Ignore Remove-Item build script errors

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 # Build, copy dll, and run gdscript integration tests
 Push-Location $PSScriptRoot 
-Remove-Item "./test/project/addons/algodot/lib/*.*"
-Remove-Item "./target/build/*" -Recurse
+Remove-Item "./test/project/addons/algodot/lib/*.*" -ErrorAction SilentlyContinue
+Remove-Item "./target/build/*" -Recurse -ErrorAction SilentlyContinue
 Copy-Item -Path "./test/project/addons" -Destination "./target/build" -Recurse
 cargo build -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
 cross build --target x86_64-unknown-linux-gnu  -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
