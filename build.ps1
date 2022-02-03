31925f4a2f67627b503e052e14a682417eb004b1
Lucas Van Mol <lucas.vanmol@gmail.com>
2022-02-02 21:42:32 -0800

    Try workflow CD

--- build.ps1
+++ build.ps1
@@ -1,7 +1,9 @@
 # Build, copy dll, and run gdscript integration tests
 Push-Location $PSScriptRoot 
 Remove-Item "./test/project/addons/algodot/lib/*.*"
+Remove-Item "./target/build/*" -Recurse
+Copy-Item -Path "./test/project/addons" -Destination "./target/build" -Recurse
 cargo build -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
 cross build --target x86_64-unknown-linux-gnu  -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
-& $Env:GODOT_EXE_PATH --path ./test/project/
+#& $Env:GODOT_EXE_PATH --path ./test/project/
 Pop-Location
