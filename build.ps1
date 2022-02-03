c87c9115ec51a4fe46235e58121abbf5f3f604a4
Lucas Van Mol <lucas.vanmol@gmail.com>
2022-02-02 22:46:14 -0800

    Build docker image for cross

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ Push-Location $PSScriptRoot
 Remove-Item "./test/project/addons/algodot/lib/*.*" -ErrorAction SilentlyContinue
 Remove-Item "./target/build/*" -Recurse -ErrorAction SilentlyContinue
 Copy-Item -Path "./test/project/addons" -Destination "./target/build" -Recurse
+docker build -t algodot-buildtarget/gnu:0.2.1 ./docker
 cargo build -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
 cross build --target x86_64-unknown-linux-gnu  -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
 #& $Env:GODOT_EXE_PATH --path ./test/project/
