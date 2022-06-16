33d26665e58d22ebb58105a316d78eae7b04d02b
Lucas Van Mol <lucas.vanmol@gmail.com>
2022-06-16 21:22:20 +0200

    Reconfigure cross dockerfile

--- build.ps1
+++ build.ps1
@@ -8,8 +8,7 @@ Copy-Item -Path "./test/project/addons" -Destination "./target/build" -Recurse
 cargo build -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
 
 # Build for linux with cross
-docker build -t algodot-buildtarget/gnu:0.2.1 ./docker
-cross build --target x86_64-unknown-linux-gnu -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib"
+cross build --target x86_64-unknown-linux-gnu -Z unstable-options --release --out-dir "./test/project/addons/algodot/lib" -v
 
 # Uncomment to run gdscript integration tests as in `./test/test.ps1`
 #& $Env:GODOT_EXE_PATH --path ./test/project/
