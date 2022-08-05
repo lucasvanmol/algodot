4cd79a5a668cf270764ae52babf297c93194e0f4
Sam2much96 <Sam2much96>
2022-08-05 05:40:38 +0100

    modified:   algodot/test/project/Scene.tscn 	new file:   algodot/test/project/addons/algodot/Algod.gd 	modified:   algodot/test/project/addons/algodot/Documentation/Content/Algorand.md 	new file:   algodot/test/project/addons/algodot/icon.png 	new file:   algodot/test/project/addons/algodot/icon.png.import 	modified:   algodot/test/project/addons/algodot/plugin.gd 	modified:   algodot/test/project/project.godot

--- algodot/test/project/addons/algodot/plugin.gd
+++ algodot/test/project/addons/algodot/plugin.gd
@@ -5,13 +5,11 @@ var gdn
 
 #########for built in documentation
 var _editor_view
-var show_documentation : bool
-#var _parts_inspector
-#var _export_plugin
+
 func _init():
 	add_autoload_singleton('DocsHelper', "res://addons/algodot/Documentation/Scripts/DocsHelper.gd")
 	#####for buiilt in documentation
-	#if get_editor_interface().is_plugin_enabled("algodot"):
+	
 	_add_custom_editor_view()
 	
 	"Adds a Custom Tab for Documentations"
@@ -19,7 +17,7 @@ func _init():
 	
 	get_editor_interface().get_inspector().get_parent_control().get_parent().add_child(_editor_view)
 	
-	#get_editor_interface().get_inspector().add_child(_editor_view)
+	
 
 
 func _enter_tree():
@@ -49,15 +47,14 @@ func _enter_tree():
 
 
 func _exit_tree():
-	# Clean up
+	
 	remove_custom_type("Algod")
 	remove_autoload_singleton("AsyncExecutorDriver")
 ##################For builtin Documentation
 	
 	remove_autoload_singleton("DocsHelper")
 	_remove_custom_editor_view()
-#	remove_inspector_plugin(_parts_inspector)
-#	remove_export_plugin(_export_plugin)
+
 
 
 
@@ -65,18 +62,15 @@ func _exit_tree():
 func get_plugin_name():
 	return "Algodot"
 
-#func make_visible(visible):
-#	if _editor_view:
-#		_editor_view.visible = visible
 
 func _add_custom_editor_view(): # for builtin documentatiion
 	_editor_view = preload("res://addons/algodot/Documentation/Scripts/DocumentationViewer.tscn").instance()
-	#_editor_view.plugin_reference = self
+
 	
 
 
 func _remove_custom_editor_view():
 	if _editor_view:
-		#remove_control_from_bottom_panel(_editor_view)
+		
 		remove_control_from_docks(_editor_view)
 #		_editor_view.queue_free()
