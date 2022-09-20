6c6df6fbe70939d4f84cb2694de25d4d91a6bb95
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-09-20 21:03:53 +0100

    Update lib.rs

--- algodot/src/lib.rs
+++ algodot/src/lib.rs
@@ -50,7 +50,7 @@ impl AsyncExecutorDriver {
 #[methods]
 impl AsyncExecutorDriver {
     #[method]
-    fn _process(&self, _owner: Node, _delta: f64) {
+    fn _process(&self, _owner: &Node, _delta: f64) {
         EXECUTOR.with(|e| {
             self.runtime
                 .block_on(async {
