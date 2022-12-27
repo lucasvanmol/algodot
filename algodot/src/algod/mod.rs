90b860ba23291d903ea005450140e15f700a04df
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-27 20:08:44 +0100

    Update mod.rs

--- algodot/src/algod/mod.rs
+++ algodot/src/algod/mod.rs
@@ -279,7 +279,7 @@ impl Algodot {
         params: SuggestedTransactionParams,
         sender: Address,
         app_id: u64,
-        app_arguments: String, 
+        #[opt] app_arguments: Option<String>, 
         
    
     ) -> Transaction {       //unwraps into MyTransaction() From method in Core.rs
@@ -288,7 +288,7 @@ impl Algodot {
             TransactionType::ApplicationCallTransaction(CallApplication {
                 sender: *sender,
                 app_id,
-                app_arguments.into_bytes(),
+                app_arguments.expect("REASON").into_bytes(),
             }),
         )
         .build()?
