cd18c61b07e4fd54c84c7b7d6b67994a40e3da0d
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-27 08:55:10 +0100

    Update mod.rs

--- algodot/src/algod/mod.rs
+++ algodot/src/algod/mod.rs
@@ -281,15 +281,13 @@ impl Algodot {
         app_id: u64,
         #[opt] app_arguments: Option<String>, 
         
-        
-        arg_as_bytes : Vec<u8> = app_arguments.expect("REASON").into_bytes();
    
     ) -> Transaction {
 
         TxnBuilder::with( 
             &params,
             CallApplication::new(*sender,app_id)
-                .app_arguments(vec![arg_as_bytes])
+                .app_arguments(vec![app_arguments.expect("REASON").into_bytes()])
                 .build(),
         )
         .build()
