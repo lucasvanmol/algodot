8395337a78fd3e0a64117167cfcd59821f0374ce
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-28 11:17:02 +0100

    Update mod.rs

--- algodot/src/algod/mod.rs
+++ algodot/src/algod/mod.rs
@@ -281,14 +281,17 @@ impl Algodot {
         #[opt] app_arguments: Option<String>, 
         
    
-    ) -> TxnBuilder::with(
+    ) -> Transaction { 
+        TxnBuilder::with(
             &params,
-            CallApplication::new(*sender, app_id)
+            CallApplication::new(*mut sender, app_id)
                 .app_arguments( vec![app_arguments.expect("REASON").into_bytes()])
                 .build(),
             )
-            .build()?;
-    
+            .build()
+            .unwrap()
+            .into()
+    }
 
     #[method]
     fn construct_asset_opt_in(
@@ -322,7 +325,7 @@ impl Algodot {
         let result = TxGroup::assign_group_id(txns_mut_refs.as_mut_slice());
         godot_unwrap!(result).map(|_| txns)
     }
-
+}
 
 asyncmethods!(algod, node, this,
     fn health(_ctx, _args) {
