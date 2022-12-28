fbec5554e6d2a05ff5e3f1d3faa4c281960871aa
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-28 11:07:31 +0100

    Update mod.rs

--- algodot/src/algod/mod.rs
+++ algodot/src/algod/mod.rs
@@ -281,17 +281,14 @@ impl Algodot {
         #[opt] app_arguments: Option<String>, 
         
    
-    ) -> Transaction {       //unwraps into MyTransaction() From method in Core.rs
-        TxnBuilder::with(
+    ) -> TxnBuilder::with(
             &params,
             CallApplication::new(*sender, app_id)
                 .app_arguments( vec![app_arguments.expect("REASON").into_bytes()])
                 .build(),
             )
             .build()?;
-         }
-        
-    }
+    
 
     #[method]
     fn construct_asset_opt_in(
