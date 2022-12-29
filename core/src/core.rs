400d1c36d55066e8bd9f898cfeef94b16765c57a
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-29 15:50:00 +0100

    Update core.rs

--- core/src/core.rs
+++ core/src/core.rs
@@ -229,7 +229,9 @@ impl ToVariant for MyTransaction {
                     //dict.insert( "txn", MyApplCallTransaction::from(appl));
                     let w = Dictionary::new();  
                     let y = VariantArray::new();
-                    y.push([105, 110, 99]);
+                    y.push(105);
+                    y.push(110);
+                    y.push(99);
                     let z = ByteArray::from_variant_array(y); //bytearray fix for txn signing //hacky fix
                    
                     //x.push(appl.app_arguments.as_ref().unwrap().clone());
