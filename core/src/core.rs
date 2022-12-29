a1db838f71e38dcd3e8139f6181b5d4011ed5070
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-29 16:04:35 +0100

    Update core.rs

--- core/src/core.rs
+++ core/src/core.rs
@@ -227,9 +227,17 @@ impl ToVariant for MyTransaction {
                 //should be further customized to include ClearState,CloseOut,DeleteApplication
                 TransactionType::ApplicationCallTransaction(appl) => { 
                     //dict.insert( "txn", MyApplCallTransaction::from(appl));
-                    let w = Dictionary::new();   
+                    let w = Dictionary::new(); 
+                    
+                    let z = ByteArray::new();
+                    z.push(105);
+                    z.push(110);
+                    z.push(99);
+                    
+                    
                     dict.insert( "app_id", appl.app_id);
-                    dict.insert("app_arg",appl.app_arguments.as_ref().unwrap().clone()); //default app arg. Works                   
+                    //dict.insert("app_arg",appl.app_arguments.as_ref().unwrap().clone()); //default app arg. Works                   
+                    dict.insert("app_arg", z);
                     dict.insert( "txn", w);
                     dict.insert( "snd", MyAddress::from(appl.sender));
                     "appl"
