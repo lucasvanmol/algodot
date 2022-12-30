d784e6c32ef8ff57d22e29f88fba6b57c1e09cba
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-12-30 11:18:08 +0100

    Update core.rs

--- core/src/core.rs
+++ core/src/core.rs
@@ -238,7 +238,7 @@ impl ToVariant for MyTransaction {
                     dict.insert( "app_id", appl.app_id);
                     //dict.insert("app_arg",appl.app_arguments.as_ref().unwrap().clone()); //default app arg. Works                   
                     //dict.insert("app_arg", ByteArray::new());
-                    dict.insert("app_arg", q);
+                    dict.insert("app_arg", &q);
                     dict.insert( "txn", w);
                     dict.insert( "snd", MyAddress::from(appl.sender));
                     "appl"
